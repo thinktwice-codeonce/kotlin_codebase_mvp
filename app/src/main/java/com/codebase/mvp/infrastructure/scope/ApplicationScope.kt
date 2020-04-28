f007f6342f98b3860389ca01497fa4aecf66f794
Phong <phongit168@gmail.com>
2020-04-28 14:47:49 +0700

    Update scope annotations.

--- app/src/main/java/com/codebase/mvp/infrastructure/scope/ApplicationScope.kt
+++ app/src/main/java/com/codebase/mvp/infrastructure/scope/ApplicationScope.kt
@@ -1,4 +1,8 @@
 package com.bss.codebase.infrastructure.scope
 
+import javax.inject.Scope
+
+@Scope
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
 annotation class ApplicationScope {
 }
